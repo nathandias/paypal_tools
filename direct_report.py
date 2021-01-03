0b27919798f978b3fe308088c53052ff2481cd79
Nathan Dias <nathan.dias@gmail.com>
2021-01-02 23:40:30 -0800

    get all transactions from pages of results

--- direct_report.py
+++ direct_report.py
@@ -10,6 +10,8 @@ parser = argparse.ArgumentParser(description="Generate PayPal reports using the 
 parser.add_argument('-m', '--server_mode', dest='mode', choices=('sandbox', 'live'), default='live')
 parser.add_argument('-c', '--config_file', dest='config_filename', default='paypal_tools.conf', action="store")
 parser.add_argument('-t', '--txn_id', dest='txn_id', action="store")
+parser.add_argument('-s', '--start_date', dest='start_date', action="store")
+parser.add_argument('-e', '--end_date', dest='end_date', action="store")
 args = parser.parse_args()
 
 # set up environment: live (default) or sandbox (for testing)
@@ -105,12 +107,79 @@ def get_full_transaction_details(txn_id, start_date, end_date):
     except Exception as msg:
         sys.exit(msg)
 
+# def get_transactions(start_date, end_date):
+#     """
+#     Get all transactions in a specified date range
+#     """
 
+#     while start_date <= end_date:
+#         if (end_date - start_date) <= 31 days # last frame, do it and break the loop
+#             transactions = get_paged_transactions(start_date, end_date)
+#             # get all transaction pages from start_date to end_date and add the transactions to the list
+#             break
+#         else:
+#             # get all transactions from start_date to (start_date+31) and add to the transactions list
+#             # start_date = start_date + 31
+#     else:
+#         # input error: start_date > end_date
+
+def get_paged_transactions(start_date, end_date):
+
+    # expecting start_date <= end_date and, end_date - start_date <= 31
+
+    print(f'"*** Getting transaction pages for date range start_date = {start_date} to end_date = {end_date} ***')
+
+    global base_url, access_headers
+    endpoint = base_url + "/v1/reporting/transactions?"
+
+    parameters = {
+        'start_date' : start_date.strftime("%Y-%m-%dT%H:%M:%S-0000"),
+        'end_date' : end_date.strftime("%Y-%m-%dT%H:%M:%S-0000"),
+        'page_size' : 500,
+        'balance_affecting_records_only' : 'Y',
+    }
+
+    endpoint += '&'.join(f'{i[0]}={i[1]}' for i in parameters.items()) # join the url parameters
+
+    page = 1
+    pages = []
+
+    query_url = endpoint + f'&page={page}'
+
+    response = requests.get(query_url, headers=access_headers)
+    results = response.json()
+    total_pages = results['total_pages']
+
+    #print("Results page", page, "of", total_pages)
+    #pprint(results)
+    pages += results['transaction_details']
+
+    while (page < total_pages):
+        page += 1
+        query_url = endpoint + f'&page={page}'
+        response = requests.get(query_url, headers=access_headers)
+        results = response.json()
+        #print("Results page", page, "of", total_pages)
+        #pprint(results)
+        pages += results['transaction_details']
+
+    print("Number of transactions:", len(pages))
+    pprint(pages)
+    
+    return pages
 
 if (args.txn_id is not None): 
     capture_result = get_capture_details(args.txn_id)
     start_date = end_date = capture_result['create_time']
     transaction_result = get_full_transaction_details(args.txn_id, start_date, end_date)
+
+elif (args.start_date is not None) and (args.end_date is not None):
+    pass
+else: # return last 31 days of transactions default if no other options specified
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=31)
+    get_transaction_pages(start_date, end_date)
+
 
 
 
